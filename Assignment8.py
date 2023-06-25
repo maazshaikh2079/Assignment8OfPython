ad2706399f45816312e25f0bc6b191f77295318a
maazshaikh2079 <124486797+maazshaikh2079@users.noreply.github.com>
2023-06-25 14:30:59 +0530

    Assignment8.py

--- Assignment8.py
+++ Assignment8.py
@@ -18,59 +18,58 @@
 # Date   : 22-06-2023
 
 # Program:-
-class PrivateAccessError(Exception):
-    
-    def __init__(self,msg):
-        super().__init__(msg);
 
 class A:
     
-    __a=None # Private
-    _b=None  # Protected
-    c=None   # Public
-    
     def __init__(self,a,b,c):
-        self.a=a
-        self.b=b
+        self.__a=a
+        self._b=b
         self.c=c
     
     def display(self):
-        print("a =",self.a)
-        print("b =",self.b)
+        print("a =",self.__a)
+        print("b =",self._b)
         print("c =",self.c);
 
 class B(A):
-
+    
     def __init__(self,a,b,c):
         super().__init__(a,b,c)
-    
-    def display(self):
-        print("[Printing by display() of 'B' class :-]");
+        self.paeA=self.PrivateAccessError("Private members of 'A' cannot be accessed.");
 
-        try:
-           if(self.a==self.a):
-               raise PrivateAccessError("Private member cannot be access.")
-           else:
-               print("a =",self.a);
-        except PrivateAccessError as pae:
-           print(f"{type(pae).__name__} : {pae.args[0]}");
+    class PrivateAccessError(Exception):
+        # def __init__(self, msg):
+        #     super().__init__(msg);
+        print(end='')
+            
+    
+    def display(self,expClsNm):
+        print("[Printing by display() of 'B' class :-]");
         
-        print("b =",self.b)
+        try:
+            print("a =",self.__a);
+        
+        except Exception as e:
+           print(f"{type(e).__name__} : {e}.");  # Pre-defined exception and msg.
+           print(f"{expClsNm} : {self.paeA}");   # User-defined exception and msg.
+        
+        print("b =",self._b)
         print("c =",self.c)
-        print()
+        print() 
         print("[Printing by display() of 'A' class :-]");
         super().display();
         
 # Driver code:- 
 b=B(10,20,15);
-b.display();
+b.display(b.paeA.__class__.__name__);
 
 # Output:-
 # [Printing by display() of 'B' class :-]
-# PrivateAccessError : Private member cannot be access.
+# AttributeError : 'B' object has no attribute '_B__a'.
+# PrivateAccessError : Private members of 'A' cannot be accessed.
 # b = 20
 # c = 15
-# 
+
 # [Printing by display() of 'A' class :-]
 # a = 10
 # b = 20
